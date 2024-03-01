8d398f2e8bcaf01e87d7d7ecea16522d56366b3b
code_hopper <ameykhondekar01@gmail.com>
2024-03-01 18:23:39 +0530

    started using js

---
+++ JS/script.js
@@ -0,0 +1,73 @@
+// alert("hello from script js!")
+
+// variables
+// scope name = value;
+
+// comment in js
+
+/*
+this
+is
+multi
+line 
+commet
+*/
+
+// let a = "10"
+// let b = 10
+// let c = 30
+
+// oprators
+
+// 1. Airhtmatics + - / * %
+// 2. increement and decreement ++ , -- (pre,post)
+// 3. Assignment =
+// 4. Conditional/Comparision Oprators < , > , <= , >= , != , == , ===
+// 5. (Logics gates) Logical Oprators &&(AND) , ||(OR) , !(NOT) 
+// 6. Bitwise oprators & , ||
+// 7. Dot notation ' . '
+
+// opration
+
+// () , [] , {}
+
+// data types in js
+// 1)primitve , 2)non-primitve
+
+// 1)  Number (integers, floats, double) (1,1.0,1.514) , String (group of charchters ,a string has to be writing in qoutes "" , '' , `` back ticks ), Boolean (true, false)
+
+// undefined (trying to access before ), null
+
+// 2) Array, objects, function
+// NaN (not a number)
+
+// let z = null
+
+let a = 30
+let b = "amey"
+let c = false
+
+let d = [1,2,3,4]
+let e = {
+    name: "name 1"
+}
+
+let f = () =>{
+    console.log('hello')
+}
+
+let g;
+
+let h = null
+
+let i = Number("a1")
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+console.log(typeof(f))
+console.log(typeof(g))
+console.log(typeof(h))
+console.log(i)
