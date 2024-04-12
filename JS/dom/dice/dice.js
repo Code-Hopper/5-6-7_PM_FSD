b639f1882fe404108d3b7d13687945e73629aace
code_hopper <ameykhondekar01@gmail.com>
2024-04-12 18:23:39 +0530

    dice game

---
+++ JS/dom/dice/dice.js
@@ -0,0 +1,25 @@
+let rollBtn = document.getElementById("roll-dice")
+
+let circles = document.getElementsByClassName("circle")
+
+rollBtn.onclick = () => {
+
+    // genrate a random number
+
+    let randomNumber = Math.floor(Math.random() * 6)
+
+    console.log(randomNumber)
+
+    document.getElementById("dice").style.animation = "rollDice 0.4s ease"
+
+    // show circle equal to random number and hide others
+
+    for(let i = 0 ; i <= 5 ; i++){
+        if (i <= randomNumber) {
+            circles[i].style.display = "block"
+        }else{
+            circles[i].style.display = "none"
+        }
+    }
+
+}
